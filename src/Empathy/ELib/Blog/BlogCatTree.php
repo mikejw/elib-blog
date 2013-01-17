3fb0b69a0fcf882f2b9988ef412b69599477e2a3
Mike Whiting <mail@mikejw.co.uk>
2013-01-17 14:30:18 +0000

    commenting and cleaning up (simplifiying) class.

--- src/Empathy/ELib/Blog/BlogCatTree.php
+++ src/Empathy/ELib/Blog/BlogCatTree.php
@@ -1,11 +1,37 @@
 <?php
 
-namespace Empathy\ELib\Blog;
+/**
+* Empathy/ELib/Blog/BlogCatTree.php
+*
+* PHP Version 5
+*
+* LICENSE: This source file is subject to the LGPLv3 License that is bundled
+* with this source code in the file licence.txt
+*
+* @category  Application
+* @package   ELib_Blog
+* @author    Mike Whiting <mail@mikejw.co.uk>
+* @copyright 2008-2013 Mike Whiting
+* @license   http://www.gnu.org/licenses/gpl-3.0-standalone.html GPL v3.0
+* @link      http://empathyphp.co.uk
+*
+*/
 
+namespace Empathy\ELib\Blog;
 use Empathy\ELib\Tree;
 
+/**
+* Class for generating blog categories tree data
+*
+* @category Application
+* @package  ELib_Blog
+* @author   Mike Whiting <mail@mikejw.co.uk>
+* @license  http://www.gnu.org/licenses/gpl-3.0-standalone.html GPL v3.0
+* @link     http://empathyphp.co.uk
+*/
 class BlogCatTree extends Tree
 {
+    
     private $blog_category;
     private $data;
     private $blog_category_ancestors;
@@ -16,35 +42,33 @@ class BlogCatTree extends Tree
     }
 
 
-    public function __construct($blog_category, $current_is_dir, $collapsed)
+    public function __construct($blog_category)
     {
+       
+
         $this->blog_category = $blog_category;
         $this->blog_category_ancestors = array(0);
         $this->banner = new \stdClass(); // quick fix (code needs cleaning up)
         
-        if ($current_is_dir) {
-            $current_id = $this->blog_category->id;
-            if (!$collapsed) {
-                array_push($this->blog_category_ancestors, $this->blog_category->id);
-            }
-        } else {
-            $current_id = $this->banner->id;
-            array_push($this->blog_category_ancestors, $this->banner->blog_category_id);
-        }
+      
+        $current_id = $this->blog_category->id;
+        array_push($this->blog_category_ancestors, $this->blog_category->id);
 
+        
         $ancestors = array();
-        if (!isset($this->blog_category->id)) {
-            $ancestors = $this->blog_category->getAncestorIDs($this->banner->blog_category_id, $ancestors);
-        } elseif ($this->blog_category->id != 0) {
+       
+        if ($this->blog_category->id != 0) {
+
             $ancestors = $this->blog_category->getAncestorIDs($this->blog_category->id, $ancestors);
         }
         if (sizeof($ancestors) > 0) {
+
             $this->blog_category_ancestors = array_merge($this->blog_category_ancestors, $ancestors);
         }
 
         $this->data = $this->buildTree(0, $this);
 
-        $this->markup = $this->buildMarkup($this->data, 0, $current_id, 0, 0, $current_is_dir);
+        $this->markup = $this->buildMarkup($this->data, 0, $current_id, 0, 0);
     }
 
     public function buildTree($id, $tree)
@@ -55,7 +79,7 @@ class BlogCatTree extends Tree
         return $nodes;
     }
     
-    private function buildMarkup($data, $level, $current_id, $last_id, $last_node_data, $current_is_dir)
+    private function buildMarkup($data, $level, $current_id, $last_id, $last_node_data)
     {
         $markup = "\n<ul";
         
@@ -87,7 +111,7 @@ class BlogCatTree extends Tree
             $children = sizeof($value['children']);
             $markup .= "<li";
 
-            if ($current_id == $value['id'] && $value['banner'] != $current_is_dir) {
+            if ($current_id == $value['id']) {
                 $markup .= " class=\"current\"";
             }
 
@@ -103,13 +127,13 @@ class BlogCatTree extends Tree
             }
             $markup .= "<img src=\"http://".WEB_ROOT.PUBLIC_DIR."/elib/$folder\" alt=\"\" />\n";
 
-            if ($current_id == $value['id'] && $value['banner'] != $current_is_dir) {
+            if ($current_id == $value['id']) {
                 $markup .= "<span class=\"label current\">".$value['label']."</span>";
             } else {
                 $markup .= "<span class=\"label\"><a href=\"http://".WEB_ROOT.PUBLIC_DIR."/admin/$url/".$value['id']."\">".$value['label']."</a></span>";
             }
             if ($children > 0) {
-                $markup .= $this->buildMarkup($value['children'], $level, $current_id, $value['id'], $value['banner'], $current_is_dir);
+                $markup .= $this->buildMarkup($value['children'], $level, $current_id, $value['id'], $value['banner']);
             }
             $markup .= "</li>\n";
         }
