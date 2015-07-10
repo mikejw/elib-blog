6f63f6c1a4e6265e05c8311609e28a030d2b06e9
Mike Whiting <mail@mikejw.co.uk>
2015-07-10 17:44:58 +0100

    adding gen class.

---
+++ src/Empathy/ELib/Gen/BlogAdmin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Empathy\ELib\Gen;
+
+class BlogAdmin extends \Empathy\MVC\Util\ControllerGen
+{
+    protected $name = 'blog';
+    protected $module = 'admin';
+    protected $parent = '\Empathy\ELib\Blog\Controller';
+}
+
+
