a479106437fd1b2f2e67ac3081dfc56fec0b7e6f
Mike Whiting <mail@mikejw.co.uk>
2016-03-09 19:06:15 +0000

    adding feed controller.

---
+++ src/Empathy/ELib/Gen/Feed.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Empathy\ELib\Gen;
+
+class Blog extends \Empathy\MVC\Util\ControllerGen
+{
+    protected $name = 'feed';
+    protected $module = 'feed';
+    protected $parent = '\Empathy\ELib\Blog\FeedController';
+}
+
