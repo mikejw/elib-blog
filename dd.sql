5c81fcd44e69e1bd017c68342b962d45370ff5aa
Mike Whiting <mail@mikejw.co.uk>
2024-02-29 18:30:35 +0000

    small fix

--- dd.sql
+++ dd.sql
@@ -18,7 +18,7 @@ id                      INT(11) AUTO_INCREMENT PRIMARY KEY,
 blog_id                 INT(11) NOT NULL,
 body                    TEXT NULL,
 meta                    TEXT NULL,
-stamp                   TIMESTAMP NULL DEFAULT NULL
+stamp                   TIMESTAMP NULL DEFAULT NULL,
 FOREIGN KEY (blog_id) REFERENCES blog (id))  ENGINE = InnoDB;
 
 
